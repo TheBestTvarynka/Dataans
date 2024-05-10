0a73a9f1a3052c9a851e3784381c2b5598e71a49
Pavlo Myroniuk <pavlo.myroniuk@apriorit.com>
2024-05-11 00:41:57 +0300

    Tray icon and opening by shortcut functionality (#1)
    * feat(totes): implement try icon functionallity.
    
    ALl functionallity is added based on this guide:
    * https://tauri.app/v1/guides/features/system-tray
    
    Currently it works well on Windows. I haven't tested it on Linux.
    
    * feat(totes): add `rustdmt.toml` files;
    
    * feat(totes): implement opening and hiding app on shortcut.
    
    The implementation is based on this answer:
    https://github.com/tauri-apps/tauri/discussions/5281#discussioncomment-3733845.
    The shortcut itself if hardcoded and can't be configured. We plan to
    change it in the future.
    It works perfectly on Windows. I haven't tested it on Linux.

--- totes/src-tauri/src/main.rs
+++ totes/src-tauri/src/main.rs
@@ -7,9 +7,88 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust (Tauri)!", name)
 }
 
+use tauri::{
+    AppHandle, CustomMenuItem, GlobalShortcutManager, Manager, Result, RunEvent, SystemTray, SystemTrayEvent,
+    SystemTrayMenu, SystemTrayMenuItem,
+};
+
+const MAIN_WINDOW_NAME: &str = "main";
+
+const WINDOW_VISIBILITY_MENU_ITEM_ID: &str = "visibility";
+const WINDOW_QUIT_MENU_ITEM_ID: &str = "quit";
+const WINDOW_HIDE_TITLE: &str = "Hide";
+const WINDOW_SHOW_TITLE: &str = "Show";
+const WINDOW_QUIT_TITLE: &str = "Quit";
+
+const GLOBAL_SHORTCUT_ACCELERATOR: &str = "F1";
+
+fn toggle_app_visibility(app: &AppHandle) -> Result<()> {
+    if let Some(window) = app.get_window(MAIN_WINDOW_NAME) {
+        let item_handle = app.tray_handle().get_item(WINDOW_VISIBILITY_MENU_ITEM_ID);
+
+        if window.is_visible().unwrap_or(true) {
+            window.hide()?;
+            item_handle.set_title(WINDOW_SHOW_TITLE)?;
+        } else {
+            window.show()?;
+            item_handle.set_title(WINDOW_HIDE_TITLE)?;
+        }
+    } else {
+        println!("{MAIN_WINDOW_NAME} window not found!");
+    }
+
+    Ok(())
+}
+
 fn main() {
+    let quit = CustomMenuItem::new(WINDOW_QUIT_MENU_ITEM_ID.to_string(), WINDOW_QUIT_TITLE);
+    let hide = CustomMenuItem::new(WINDOW_VISIBILITY_MENU_ITEM_ID.to_string(), WINDOW_HIDE_TITLE);
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide);
+
+    let tray = SystemTray::new().with_menu(tray_menu);
+
     tauri::Builder::default()
+        .system_tray(tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick { .. } | SystemTrayEvent::DoubleClick { .. } => {
+                toggle_app_visibility(app).unwrap()
+            }
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                WINDOW_QUIT_MENU_ITEM_ID => {
+                    std::process::exit(0);
+                }
+                WINDOW_VISIBILITY_MENU_ITEM_ID => toggle_app_visibility(app).unwrap(),
+                _ => {}
+            },
+            _ => {}
+        })
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::CloseRequested { api, .. } => {
+                event.window().hide().unwrap();
+                api.prevent_close();
+            }
+            _ => {}
+        })
         .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .build(tauri::generate_context!())
+        .expect("error while building tauri application")
+        .run(|app_handle, event| match event {
+            RunEvent::Ready => {
+                let app_handle = app_handle.clone();
+                app_handle
+                    .global_shortcut_manager()
+                    .register(GLOBAL_SHORTCUT_ACCELERATOR, move || {
+                        println!("global shorcut: {}", GLOBAL_SHORTCUT_ACCELERATOR);
+                        toggle_app_visibility(&app_handle).unwrap();
+                    })
+                    .unwrap();
+            }
+            RunEvent::ExitRequested { api, .. } => {
+                api.prevent_exit();
+            }
+            _ => {}
+        })
 }
