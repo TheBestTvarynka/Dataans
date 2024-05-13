1d1d77830a5069ed4c74dbdcf0980cf048dd2bd8
Pavlo Myroniuk <pavlo.myroniuk@apriorit.com>
2024-05-13 22:53:35 +0300

    feat(front): small changes;

--- totes/src-tauri/src/config.rs
+++ totes/src-tauri/src/config.rs
@@ -5,7 +5,7 @@ use common::Theme;
 #[tauri::command]
 pub fn theme() -> Theme {
     // TODO(@TheBestTvarynka): proper config file path detection.
-    let config_file_path = "../configs/theme_light.toml";
+    let config_file_path = "../configs/theme_dark.toml";
 
     let config_data = match read_to_string(config_file_path) {
         Ok(data) => data,
@@ -15,10 +15,8 @@ pub fn theme() -> Theme {
         }
     };
 
-    info!("Config file `{}` read. Config data: {}.", config_file_path, config_data);
-
     toml::from_str(&config_data).unwrap_or_else(|err| {
-        error!("Can not patse theme config: {:?}", err);
+        error!("Can not paste theme config: {:?}", err);
         Default::default()
     })
 }
