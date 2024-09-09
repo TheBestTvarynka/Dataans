531a65d3bf7fca821826af0a1a4bf2781cf6b7f4
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2024-09-09 10:13:32 +0300

    feat(front): add close button for AppInfo window ;

--- dataans/src/spaces/app_info/app_info_window.rs
+++ dataans/src/spaces/app_info/app_info_window.rs
@@ -7,6 +7,13 @@ pub fn AppInfoWindow(#[prop(into)] close: Callback<(), ()>) -> impl IntoView {
 
     view! {
         <div class="app-into-window">
+            <button
+                class="tool app-window-close-button"
+                title="Close window"
+                on:click=move |_| close.call(())
+            >
+                <img alt="edit note" src="/public/icons/cancel.png" />
+            </button>
             <span class="app-into-window-title">{format!("Dataans v.{}", env!("CARGO_PKG_VERSION"))}</span>
             <span>"Take notes in the form of markdown snippets grouped into spaces."</span>
             <span>"Source code: "<a href="https://github.com/TheBestTvarynka/Dataans">"GitHub/TbeBestTvarynka/Dataans"</a></span>
