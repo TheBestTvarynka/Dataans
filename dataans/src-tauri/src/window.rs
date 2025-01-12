26e3c37b0faefa95e154cc07f7b1a72b15161a04
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2025-01-12 15:28:24 +0200

    fix(back): auth window creation (just show it if already exist);

--- dataans/src-tauri/src/window.rs
+++ dataans/src-tauri/src/window.rs
@@ -9,18 +9,19 @@ pub async fn open_auth_window(app: AppHandle) -> CommandResultEmpty {
     if let Some(window) = app.webview_windows().get(AUTH_WINDOW_TITLE) {
         warn!("Auth window already opened. Destroying...");
 
-        window.destroy().map_err(|err| CommandError::Tauri(err.to_string()))?;
+        window.show().map_err(|err| CommandError::Tauri(err.to_string()))?;
+        window.set_focus().map_err(|err| CommandError::Tauri(err.to_string()))?;
+    } else {
+        WebviewWindowBuilder::new(&app, AUTH_WINDOW_TITLE, WebviewUrl::App("auth".into()))
+            .always_on_top(false)
+            .decorations(true)
+            .closable(true)
+            .focused(true)
+            .inner_size(800.0, 800.0)
+            .title("Account")
+            .build()
+            .map_err(|err| CommandError::Tauri(err.to_string()))?;
     }
-
-    WebviewWindowBuilder::new(&app, AUTH_WINDOW_TITLE, WebviewUrl::App("auth".into()))
-        .always_on_top(false)
-        .decorations(true)
-        .closable(true)
-        .focused(true)
-        .inner_size(800.0, 800.0)
-        .title("Account")
-        .build()
-        .map_err(|err| CommandError::Tauri(err.to_string()))?;
 
     Ok(())
 }
