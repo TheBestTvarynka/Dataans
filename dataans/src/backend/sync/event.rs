e7da19e539212cafa72ee455081c674206da1f25
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2025-01-19 13:38:41 +0200

    feat(front): update events listening code;

--- dataans/src/backend/sync/event.rs
+++ dataans/src/backend/sync/event.rs
@@ -1,7 +1,9 @@
 // MIT License
 //
 // Copyright (c) 2022 Jonas Kruckenberg
-// https://github.com/JonasKruckenberg/tauri-sys/blob/e953a9c912212d43fa42add71bc84c40c30140e0/src/event.rs
+// https://github.com/JonasKruckenberg/tauri-sys/blob/6c75037edd06e4c39d972a7897a87fc52500c511/src/event.rs
+
+#![allow(dead_code)]
 
 use std::fmt::Debug;
 
@@ -13,17 +15,46 @@ use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::Closure;
 use wasm_bindgen::JsValue;
 
+pub const WINDOW_RESIZED: &str = "tauri://resize";
+pub const WINDOW_MOVED: &str = "tauri://move";
+pub const WINDOW_CLOSE_REQUESTED: &str = "tauri://close-requested";
+pub const WINDOW_DESTROYED: &str = "tauri://destroyed";
+pub const WINDOW_FOCUS: &str = "tauri://focus";
+pub const WINDOW_BLUR: &str = "tauri://blur";
+pub const WINDOW_SCALE_FACTOR_CHANGED: &str = "tauri://scale-change";
+pub const WINDOW_THEME_CHANGED: &str = "tauri://theme-changed";
+pub const WINDOW_CREATED: &str = "tauri://window-created";
+pub const WEBVIEW_CREATED: &str = "tauri://webview-created";
+pub const DRAG_ENTER: &str = "tauri://drag-enter";
+pub const DRAG_OVER: &str = "tauri://drag-over";
+pub const DRAG_DROP: &str = "tauri://drag-drop";
+pub const DRAG_LEAVE: &str = "tauri://drag-leave";
+
 #[derive(Debug, Clone, PartialEq, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Event<T> {
     /// Event name
     pub event: String,
     /// Event identifier used to unlisten
-    pub id: f32,
+    pub id: isize,
     /// Event payload
     pub payload: T,
-    /// The label of the window that emitted this event
-    pub window_label: Option<String>,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize)]
+#[serde(tag = "kind", content = "label")]
+pub enum EventTarget {
+    Any,
+    AnyLabel(String),
+    App,
+    Window(String),
+    Webview(String),
+    WebviewWindow(String),
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize)]
+pub(crate) struct Options {
+    pub target: EventTarget,
 }
 
 /// Emits an event to the backend.
@@ -59,6 +90,40 @@ pub async fn emit<T: Serialize>(event: &str, payload: &T) -> CommandResult<()> {
     Ok(())
 }
 
+/// Emits an event to the backend.
+///
+/// # Example
+///
+/// ```rust,no_run
+/// use tauri_api::event::{EventTarget, emit_to};
+/// use serde::Serialize;
+///
+/// #[derive(Serialize)]
+/// struct Payload {
+///     logged_in: bool,
+///     token: String
+/// }
+///
+/// emit_to(EventTarget::Any, "frontend-loaded", &Payload { logged_in: true, token: "authToken" }).await;
+/// ```
+///
+/// @param event Event name. Must include only alphanumeric characters, `-`, `/`, `:` and `_`.
+#[inline(always)]
+pub async fn emit_to<T: Serialize>(target: &EventTarget, event: &str, payload: &T) -> CommandResult<()> {
+    inner::emitTo(
+        serde_wasm_bindgen::to_value(target).map_err(|err| CommandError::JsValue(err.to_string()))?,
+        event,
+        serde_wasm_bindgen::to_value(payload).map_err(|err| CommandError::JsValue(err.to_string()))?,
+    )
+    .await
+    .map_err(|err| {
+        error!("{:?}", err);
+        CommandError::TauriEvent(format!("{:?}", err))
+    })?;
+
+    Ok(())
+}
+
 /// Listen to an event from the backend.
 ///
 /// The returned Future will automatically clean up it's underlying event listener when dropped, so no manual unlisten function needs to be called.
@@ -68,8 +133,9 @@ pub async fn emit<T: Serialize>(event: &str, payload: &T) -> CommandResult<()> {
 /// ```rust,no_run
 /// use tauri_api::event::listen;
 /// use web_sys::console;
+/// use futures::StreamExt;
 ///
-/// let events = listen::<String>("error");
+/// let events = listen::<String>("error", EventTarget::Any);
 ///
 /// while let Some(event) = events.next().await {
 ///     console::log_1(&format!("Got error in window {}, payload: {}", event.window_label, event.payload).into());
@@ -85,12 +151,65 @@ where
     let closure = Closure::<dyn FnMut(JsValue)>::new(move |raw| {
         let _ = tx.unbounded_send(serde_wasm_bindgen::from_value(raw).unwrap());
     });
-    let unlisten = inner::listen(event, &closure)
-        .await
-        .map_err(|err| {
-            error!("{:?}", err);
-            CommandError::TauriEvent(format!("{:?}", err))
-        })?;
+    let unlisten = inner::listen(
+        event,
+        &closure,
+        serde_wasm_bindgen::to_value(&Options {
+            target: EventTarget::Any,
+        })
+        .map_err(|err| CommandError::JsValue(err.to_string()))?,
+    )
+    .await
+    .map_err(|err| {
+        error!("{:?}", err);
+        CommandError::TauriEvent(format!("{:?}", err))
+    })?;
+    closure.forget();
+
+    Ok(Listen {
+        rx,
+        unlisten: js_sys::Function::from(unlisten),
+    })
+}
+
+/// Listen to an event from the backend.
+///
+/// The returned Future will automatically clean up it's underlying event listener when dropped, so no manual unlisten function needs to be called.
+/// See [Differences to the JavaScript API](../index.html#differences-to-the-javascript-api) for details.
+///
+/// # Example
+///
+/// ```rust,no_run
+/// use tauri_api::event::{EventTarget, listen_to};
+/// use web_sys::console;
+/// use futures::StreamExt;
+///
+/// let events = listen_to::<String>("error", EventTarget::Any);
+///
+/// while let Some(event) = events.next().await {
+///     console::log_1(&format!("Got error in window {}, payload: {}", event.window_label, event.payload).into());
+/// }
+/// ```
+#[inline(always)]
+pub async fn listen_to<T>(event: &str, target: EventTarget) -> CommandResult<impl Stream<Item = Event<T>>>
+where
+    T: DeserializeOwned + 'static,
+{
+    let (tx, rx) = mpsc::unbounded::<Event<T>>();
+
+    let closure = Closure::<dyn FnMut(JsValue)>::new(move |raw| {
+        let _ = tx.unbounded_send(serde_wasm_bindgen::from_value(raw).unwrap());
+    });
+    let unlisten = inner::listen(
+        event,
+        &closure,
+        serde_wasm_bindgen::to_value(&Options { target }).map_err(|err| CommandError::JsValue(err.to_string()))?,
+    )
+    .await
+    .map_err(|err| {
+        error!("{:?}", err);
+        CommandError::TauriEvent(format!("{:?}", err))
+    })?;
     closure.forget();
 
     Ok(Listen {
@@ -106,7 +225,7 @@ pub(crate) struct Listen<T> {
 
 impl<T> Drop for Listen<T> {
     fn drop(&mut self) {
-        debug!("Calling unlisten for listen callback");
+        log::debug!("Calling unlisten for listen callback");
         self.unlisten.call0(&wasm_bindgen::JsValue::NULL).unwrap();
     }
 }
@@ -157,12 +276,74 @@ where
     let closure: Closure<dyn FnMut(JsValue)> = Closure::once(move |raw| {
         let _ = tx.send(serde_wasm_bindgen::from_value(raw).unwrap());
     });
-    let unlisten = inner::once(event, &closure)
-        .await
-        .map_err(|err| {
-            error!("{:?}", err);
-            CommandError::TauriEvent(format!("{:?}", err))
-        })?;
+    let unlisten = inner::once(
+        event,
+        &closure,
+        serde_wasm_bindgen::to_value(&Options {
+            target: EventTarget::Any,
+        })
+        .map_err(|err| CommandError::JsValue(err.to_string()))?,
+    )
+    .await
+    .map_err(|err| {
+        error!("{:?}", err);
+        CommandError::TauriEvent(format!("{:?}", err))
+    })?;
+    closure.forget();
+
+    let fut = Once {
+        rx,
+        unlisten: js_sys::Function::from(unlisten),
+    };
+
+    fut.await
+}
+
+/// Listen to an one-off event from the backend.
+///
+/// The returned Future will automatically clean up it's underlying event listener when dropped, so no manual unlisten function needs to be called.
+/// See [Differences to the JavaScript API](../index.html#differences-to-the-javascript-api) for details.
+///
+/// # Example
+///
+/// ```rust,no_run
+/// use tauri_api::event::{EventTarget, once_to};
+/// use serde::Deserialize;
+/// use web_sys::console;
+///
+/// #[derive(Deserialize)]
+/// interface LoadedPayload {
+///   logged_in: bool,
+///   token: String
+/// }
+///
+/// # async fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// const event = once_to::<LoadedPayload>("loaded", EventTarget::Any).await?;
+///
+/// console::log_1!(&format!("App is loaded, loggedIn: {}, token: {}", event.payload.logged_in, event.payload.token).into());
+/// # Ok(())
+/// # }
+/// ```
+#[inline(always)]
+pub async fn once_to<T>(event: &str, target: EventTarget) -> CommandResult<Event<T>>
+where
+    T: DeserializeOwned + 'static,
+{
+    let (tx, rx) = oneshot::channel::<Event<T>>();
+
+    let closure: Closure<dyn FnMut(JsValue)> = Closure::once(move |raw| {
+        let _ = tx.send(serde_wasm_bindgen::from_value(raw).unwrap());
+    });
+    let unlisten = inner::once(
+        event,
+        &closure,
+        serde_wasm_bindgen::to_value(&Options { target }).map_err(|err| CommandError::JsValue(err.to_string()))?,
+    )
+    .await
+    .map_err(|err| {
+        error!("{:?}", err);
+        CommandError::TauriEvent(format!("{:?}", err))
+    })?;
     closure.forget();
 
     let fut = Once {
@@ -196,7 +377,7 @@ impl<T> Future for Once<T> {
     }
 }
 
-mod inner {
+pub mod inner {
     use wasm_bindgen::prelude::{wasm_bindgen, Closure};
     use wasm_bindgen::JsValue;
 
@@ -205,8 +386,18 @@ mod inner {
         #[wasm_bindgen(catch)]
         pub async fn emit(event: &str, payload: JsValue) -> Result<(), JsValue>;
         #[wasm_bindgen(catch)]
-        pub async fn listen(event: &str, handler: &Closure<dyn FnMut(JsValue)>) -> Result<JsValue, JsValue>;
+        pub async fn emitTo(target: JsValue, event: &str, payload: JsValue) -> Result<(), JsValue>;
         #[wasm_bindgen(catch)]
-        pub async fn once(event: &str, handler: &Closure<dyn FnMut(JsValue)>) -> Result<JsValue, JsValue>;
+        pub async fn listen(
+            event: &str,
+            handler: &Closure<dyn FnMut(JsValue)>,
+            options: JsValue,
+        ) -> Result<JsValue, JsValue>;
+        #[wasm_bindgen(catch)]
+        pub async fn once(
+            event: &str,
+            handler: &Closure<dyn FnMut(JsValue)>,
+            options: JsValue,
+        ) -> Result<JsValue, JsValue>;
     }
 }
