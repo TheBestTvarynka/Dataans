f9e5a1573657abc8dc53c6e144f5681342e31f90
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2025-06-29 12:05:36 +0300

    feat(web-server): better files directory handling: make it configurable. create if not exist;

--- crates/web-server/src/main.rs
+++ crates/web-server/src/main.rs
@@ -10,6 +10,7 @@ mod logging;
 mod routes;
 pub mod services;
 
+use std::env;
 use std::path::PathBuf;
 use std::sync::Arc;
 
@@ -21,7 +22,8 @@ use crate::db::PostgresDb;
 use crate::services::{Auth as AuthService, Data as DataService, Fs};
 
 const DATABASE_URL: &str = "DATAANS_WEB_SERVER_DATABASE_URL";
-const DATAANS_SERVER_ENCRYPTION_KEY: &str = "DATAANS_SERVER_ENCRYPTION_KEY";
+const SERVER_ENCRYPTION_KEY: &str = "DATAANS_SERVER_ENCRYPTION_KEY";
+const FILES_DIR: &str = "DATAANS_WEB_SERVER_FILES_DIR";
 
 pub struct State<D, S> {
     pub auth_service: AuthService<D>,
@@ -37,24 +39,32 @@ impl WebServerState {
             .max_connections(16)
             .min_connections(1)
             .acquire_timeout(std::time::Duration::from_secs(3))
-            .connect_lazy(&std::env::var(DATABASE_URL).expect("database url env var should be set"))
+            .connect_lazy(&env::var(DATABASE_URL).expect("database url env var should be set"))
             .expect("can not connect to postgresql db");
 
         sqlx::migrate!().run(&pool).await.expect("Failed to run migrations");
 
         let db = Arc::new(PostgresDb::new(pool));
 
-        let server_encryption_key = hex::decode(
-            std::env::var(DATAANS_SERVER_ENCRYPTION_KEY).expect("server encryption key env var should be set"),
-        )
-        .expect("server encryption key should be a valid hex string")
-        .try_into()
-        .expect("invalid server encryption key length");
+        let server_encryption_key =
+            hex::decode(env::var(SERVER_ENCRYPTION_KEY).expect("server encryption key env var should be set"))
+                .expect("server encryption key should be a valid hex string")
+                .try_into()
+                .expect("invalid server encryption key length");
+
+        let files_dir = PathBuf::from(env::var(FILES_DIR).unwrap_or_else(|_| String::from("dist")));
+        if !files_dir.exists() {
+            std::fs::create_dir_all(&files_dir)
+                .inspect_err(|err| {
+                    error!(?err, ?files_dir, "Failed to create files directory");
+                })
+                .expect("Failed to create files directory");
+        }
 
         Self {
             auth_service: AuthService::new(Arc::clone(&db), server_encryption_key),
             data_service: DataService::new(Arc::clone(&db)),
-            file_saver: Fs::new(PathBuf::from(String::from("dist"))),
+            file_saver: Fs::new(files_dir),
         }
     }
 }
