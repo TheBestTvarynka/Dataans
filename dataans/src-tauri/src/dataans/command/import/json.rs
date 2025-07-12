d0cd1dbe16717738b37e5587fb7400aec652b7a7
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2025-07-12 16:56:17 +0300

    feat(back): data import feature works again;

--- dataans/src-tauri/src/dataans/command/import/json.rs
+++ dataans/src-tauri/src/dataans/command/import/json.rs
@@ -2,12 +2,14 @@ use std::fs::File;
 use std::path::Path;
 
 use common::export::{Schema, SchemaV1};
+use common::note::{CreateNoteOwned, Note};
+use common::space::{CreateSpaceOwned, Space};
 use futures::future::try_join_all;
 use serde_json;
 
 use crate::dataans::db::Db;
-use crate::dataans::service::note::NoteService;
-use crate::dataans::service::space::SpaceService;
+use crate::dataans::service::note::{NoteService, NoteServiceError};
+use crate::dataans::service::space::{SpaceService, SpaceServiceError};
 use crate::dataans::DataansError;
 
 pub async fn import_v1<D: Db>(
@@ -19,23 +21,66 @@ pub async fn import_v1<D: Db>(
         let space = space_data.space;
         let notes = space_data.notes;
 
-        if space_service.space_by_id(space.id).await.is_err() {
-            space_service.create_space(space).await?;
+        let Space {
+            id,
+            name,
+            created_at: _,
+            updated_at,
+            avatar,
+        } = space;
+        let space_data = CreateSpaceOwned { id, name, avatar };
+
+        match space_service.space_by_id(space.id).await {
+            Err(SpaceServiceError::NotFound) => {
+                space_service.create_space(space_data).await?;
+            }
+            Ok(space) => {
+                if space.updated_at < updated_at {
+                    space_service.create_space(space_data).await?;
+                }
+            }
+            Err(err) => return Err(DataansError::from(err)),
         }
 
         let note_futures = notes.into_iter().map(|note| async move {
-            if note_service.note_by_id(note.id).await.is_err() {
-                note_service.create_note(note).await?;
+            let Note {
+                id,
+                text,
+                created_at: _,
+                updated_at,
+                files,
+                space_id,
+            } = note;
+            let note_data = CreateNoteOwned {
+                id,
+                text,
+                space_id,
+                files,
+            };
+
+            match note_service.note_by_id(note.id).await {
+                Err(NoteServiceError::NotFound) => {
+                    note_service.create_note(note_data).await?;
+                }
+                Ok(note) => {
+                    if note.updated_at < updated_at {
+                        note_service.create_note(note_data).await?;
+                    }
+                }
+                Err(err) => return Err(DataansError::from(err)),
             }
+
             Ok::<(), DataansError>(())
         });
 
         try_join_all(note_futures).await?;
+
         Ok::<(), DataansError>(())
     });
 
     // Wait for all spaces to complete
     try_join_all(space_futures).await?;
+
     Ok(())
 }
 
