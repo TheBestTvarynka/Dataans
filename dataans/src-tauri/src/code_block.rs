d2f062a42423715d02bcde783d0413b9e7a01b25
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2025-08-31 22:45:31 +0300

    feat(back): code block code parsing: add more syntaxes;

--- dataans/src-tauri/src/code_block.rs
+++ dataans/src-tauri/src/code_block.rs
@@ -5,7 +5,13 @@ use syntect::html::highlighted_html_for_string;
 use syntect::parsing::SyntaxSet;
 
 static THEMES: LazyLock<ThemeSet> = LazyLock::new(ThemeSet::load_defaults);
-static SYNTAXES: LazyLock<SyntaxSet> = LazyLock::new(SyntaxSet::load_defaults_newlines);
+
+static SYNTAXES: LazyLock<SyntaxSet> = LazyLock::new(|| {
+    use syntect::dumps::from_uncompressed_data;
+
+    from_uncompressed_data(include_bytes!("../resources/assets/default_newlines.packdump"))
+        .expect("syntax loading should not fail")
+});
 
 /// Parsed the code block value and returns generated HTML for this code.
 #[tauri::command]
@@ -18,4 +24,16 @@ pub fn parse_code(lang: String, code: String) -> String {
 
     highlighted_html_for_string(&code, &SYNTAXES, syntax, &THEMES.themes["Solarized (dark)"])
         .expect("Code HTML generation should not fail.")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn syntaxes_loading() {
+        SYNTAXES.syntaxes().iter().for_each(|s| {
+            println!("=> {:?} {:?}", s.name, s.file_extensions);
+        });
+    }
 }
