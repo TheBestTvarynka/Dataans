fd69e6974ba7892be4fe57d725d9f90ed351b2c8
Pavlo Myroniuk <the.best.tvarynka@gmail.com>
2025-08-31 23:43:27 +0300

    feat(back): improve code block syntax detection; (#127)

--- dataans/src-tauri/src/code_block.rs
+++ dataans/src-tauri/src/code_block.rs
@@ -5,14 +5,18 @@ use syntect::html::highlighted_html_for_string;
 use syntect::parsing::SyntaxSet;
 
 static THEMES: LazyLock<ThemeSet> = LazyLock::new(ThemeSet::load_defaults);
-static SYNTAXES: LazyLock<SyntaxSet> = LazyLock::new(SyntaxSet::load_defaults_newlines);
+
+static SYNTAXES: LazyLock<SyntaxSet> = LazyLock::new(|| {
+    use syntect::dumps::from_uncompressed_data;
+
+    from_uncompressed_data(include_bytes!("../resources/assets/default_newlines.packdump"))
+        .expect("syntax loading should not fail")
+});
 
 /// Parsed the code block value and returns generated HTML for this code.
 #[tauri::command]
 pub fn parse_code(lang: String, code: String) -> String {
-    let syntax = if let Some(syntax) = SYNTAXES.find_syntax_by_name(&lang) {
-        syntax
-    } else if let Some(syntax) = SYNTAXES.find_syntax_by_extension(&lang) {
+    let syntax = if let Some(syntax) = SYNTAXES.find_syntax_by_token(&lang) {
         syntax
     } else {
         SYNTAXES.find_syntax_plain_text()
@@ -20,4 +24,16 @@ pub fn parse_code(lang: String, code: String) -> String {
 
     highlighted_html_for_string(&code, &SYNTAXES, syntax, &THEMES.themes["Solarized (dark)"])
         .expect("Code HTML generation should not fail.")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn syntaxes_loading() {
+        SYNTAXES.syntaxes().iter().for_each(|s| {
+            println!("=> {:?} {:?}", s.name, s.file_extensions);
+        });
+    }
 }
